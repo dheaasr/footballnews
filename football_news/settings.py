012917a9895bab0db63924e54055b8178b660595
dhea anggrayni <dheaanggrayni@MacBook-Air-dhea.local>
2025-09-10 15:42:49 +0700

    Tutorial 2

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,6 +32,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "dhea-anggrayningsih41-footballnews.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://dhea-anggrayningsih41-footballnews.pbp.cs.ui.ac.id"]
 
 # Application definition
 
@@ -60,7 +62,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # Tambahkan konten baris ini
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
